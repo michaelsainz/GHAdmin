5b502ecfed8602653218db482e0f1e67d35ab511
Michael Sainz <michaelsainz@github.com>
2018-03-25 10:58:09 -0700

    Add Rename-GHEUser cmdlet with basic functionality

--- GHEAdmin.psm1
+++ GHEAdmin.psm1
@@ -295,6 +295,45 @@ function Resume-GHEUser {
 		Write-Debug -Message 'Exiting Function: Resume-GHEUser'
 	}
 }
+function Rename-GHEUser {
+	[CmdletBinding()]
+	Param(
+		# URL of the API end point
+		[Parameter(Mandatory = $true)]
+		[String]$ComputerName,
+
+		# Username/login of the user
+		[Parameter(Mandatory = $true)]
+		[String]$Handle,
+
+		[Parameter(Mandatory = $true)]
+		[String]$NewHandle,
+
+		# Personal Access Token for authentication against the GHE API
+		[Parameter(Mandatory = $true)]
+		[PSCredential]$Credential
+	)
+	Begin {
+		Write-Debug -Message 'Entered Function: Rename-GHEUser'
+	}
+	Process {
+		$Body = @{
+			'login' = $NewHandle
+		}
+		Write-Debug -Message "Request Body: $(Out-String -InputObject $Body)"
+
+		$JSONData = ConvertTo-Json -InputObject $Body
+		Write-Debug -Message "JSON data: $JSONData"
+
+		Write-Debug -Message "Querying for user: $Handle"
+		$WebResults = Invoke-RestMethod -Uri "https://$ComputerName/api/v3/admin/users/$Handle" -Method PATCH -Authentication Basic -Body $JSONData -Credential $Credential -SkipCertificateCheck
+
+		Write-Debug -Message "Response from endpoint: $WebResults"
+	}
+	End {
+		Write-Debug -Message 'Exiting Function: Rename-GHEUser'
+	}
+}
 function Get-GHETeam {
 	[CmdletBinding()]
 	Param(
