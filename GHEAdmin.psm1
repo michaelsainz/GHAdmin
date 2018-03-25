4f7bc94acd529ac1cb077ae49bfb100ca509eb56
Michael Sainz <michaelsainz@github.com>
2018-03-24 22:39:35 -0700

    Add Resume-GHEUser cmdlet with basic functionality

--- GHEAdmin.psm1
+++ GHEAdmin.psm1
@@ -267,6 +267,34 @@ function Suspend-GHEUser {
 		Write-Debug -Message 'Exiting Function: Suspend-GHEUser'
 	}
 }
+function Resume-GHEUser {
+	[CmdletBinding()]
+	Param(
+		# URL of the API end point
+		[Parameter(Mandatory = $true)]
+		[String]$ComputerName,
+
+		# Username/login of the user
+		[Parameter(Mandatory = $false)]
+		[String[]]$Handle,
+
+		# Personal Access Token for authentication against the GHE API
+		[Parameter(Mandatory = $true)]
+		[PSCredential]$Credential
+	)
+	Begin {
+		Write-Debug -Message 'Entered Function: Resume-GHEUser'
+	}
+	Process {
+		Foreach ($User in $Handle) {
+			Write-Debug -Message "Querying for user: $User"
+			Invoke-RestMethod -Uri "https://$ComputerName/api/v3/users/$User/suspended" -Method DELETE -Authentication Basic -Credential $Credential -SkipCertificateCheck
+		}
+	}
+	End {
+		Write-Debug -Message 'Exiting Function: Resume-GHEUser'
+	}
+}
 function Get-GHETeam {
 	[CmdletBinding()]
 	Param(
