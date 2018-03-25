9d1786f6d9034825ffe90aa6648ab0e53d1888d3
Michael Sainz <michaelsainz@github.com>
2018-03-25 16:10:28 -0700

    Add Remove-GHERepo cmdlet with basic functionality

--- GHEAdmin.psm1
+++ GHEAdmin.psm1
@@ -622,6 +622,40 @@ function Get-GHERepo {
 		Write-Debug -Message "Exited function: Get-GHERepo"
 	}
 }
+function Remove-GHERepo {
+	[CmdletBinding()]
+	Param(
+		# URL of the API end point
+		[Parameter(Mandatory = $true)]
+		[String]$ComputerName,
+
+		# Credential object for authentication against the GHE API
+		[Parameter(Mandatory = $true)]
+		[PSCredential]$Credential,
+
+		# Name of the repository
+		[Parameter(Mandatory = $true)]
+		[String[]]$Name,
+
+		# Username/login for the user/organization
+		[Parameter(Mandatory = $true)]
+		[String]$Owner
+	)
+	Begin {
+		Write-Debug -Message 'Entered Function: Remove-GHERepo'
+	}
+	Process {
+		Foreach ($Repo in $Name) {
+			Write-Debug -Message "Removing repository: $Repo"
+
+			$WebResult = Invoke-RestMethod -Uri "https://$ComputerName/api/v3/repos/$Owner/$Repo" -Method DELETE -Authentication Basic -Credential $Cred -SkipCertificateCheck
+			Write-Debug -Message "Result of REST request for the removal of the repository: $(Out-String -InputObject $WebResult)"
+		}
+	}
+	End {
+		Write-Debug -Message 'Exited Function: Remove-GHERepo'
+	}
+}
 function Set-GHERepoProperty {
 	[CmdletBinding()]
 	Param(
