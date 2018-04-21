3cfc0fa7ee532a1409a592766bfdcf471299e720
Michael Sainz <michaelsainz@github.com>
2018-04-20 22:19:09 -0700

    Add initial Start-GHRepoMigration cmdlet

--- GHAdmin/public/GHAdmin.psm1
+++ GHAdmin/public/GHAdmin.psm1
@@ -1402,3 +1402,60 @@ function Get-GHEIssue {
 		Write-Debug -Message 'Exited Function: Get-GHEIssue'
 	}
 }
+function Start-GHRepoMigration {
+	<#
+	.SYNOPSIS
+		Start a GitHub migration job
+	.DESCRIPTION
+		This cmdlet begins creating a migration job and archive of the specified repositories
+	.EXAMPLE
+		PS C:\> <example usage>
+		Explanation of what the example does
+	.INPUTS
+		Inputs (if any)
+	.OUTPUTS
+		Output (if any)
+	.NOTES
+		General notes
+	#>
+	[CmdletBinding()]
+	Param (
+		# The Personal Access Token with admin:org rights on github.com
+		[Parameter(Mandatory=$true)]
+		[Alias('GHPAT')]
+		[String]$GHPersonalAccessToken,
+
+		# Parameter help description
+		[Parameter(Mandatory=$false)]
+		[Switch]$LockRepositories = $false,
+
+		# The repositories to migrate to the GHE instance
+		[Parameter(Mandatory=$true)]
+		[String[]]$Repositories,
+
+		# Organization on Github.com
+		[Parameter(Mandatory=$true)]
+		[String]$GHOrganization
+	)
+	Begin {
+		$Headers = @{
+			'Authorization' = "token $GHPersonalAccessToken"
+			'Accept' = 'application/vnd.github.wyandotte-preview+json'
+		}
+		Write-Debug -Message "Header Body: $(Out-String -InputObject $Headers)"
+	}
+	Process {
+		$Body = @{
+			'lock_repositories' = $LockRepositories.ToBool()
+			'repositories' = $Repositories
+		}
+		$Body = ConvertTo-Json -InputObject $Body
+		Write-Debug -Message "Data Body: $(Out-String -InputObject $Body)"
+
+		Write-Debug -Message "Calling API: https://api.github.com/orgs/$GHOrganization/migrations"
+		Invoke-RestMethod -Uri "https://api.github.com/orgs/$GHOrganization/migrations" -Headers $Headers -Body $Body -Method POST
+	}
+	End {
+		Write-Debug -Message 'Exited Function: Start-GHRepoMigration'
+	}
+}
