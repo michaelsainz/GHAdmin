e6116120093fafee79610eebd78e67c567598db0
Michael Sainz <michaelsainz@github.com>
2020-05-07 16:27:39 -0700

    Revert "Conditional Test"
    This reverts commit f1de22614c1ba1325ec9d1b169a4d21f3109764c.

--- entrypoint.ps1
+++
@@ -1,4 +0,0 @@
-$dir = Get-ChildItem -Path ./
-New-Item -Path ./GHAdminTest.psm1
-Foreach-object -InputObject $dir -Process {Get-Content -Path $_.FullName | Add-Content -Path ./GHAdminTest.psm1}
-Get-Content -Path ./GHAdminTest.psm1
